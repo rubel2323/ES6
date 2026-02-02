f65ba71661aab8c9d5a2410d31408b999eafe38f
Rubel <mr.adcadre41@gmail.com>
2026-02-02 22:02:56 +0600

    key, values and entries added

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const devices = ["iPhone", "samsung", "onePlus", "nokia"];
+const [devices1, devices2, devices3, devices4] = devices;
+console.log(devices1);
+console.log(devices2);
+
+console.log(devices3);
+console.log(devices4);
+
+const user = { name: "jhone", age: 25, job: "developer" };
+const { name, age, job } = user;
+console.log(name);
+console.log(age);
+console.log(job);
---
+++ key.js
@@ -0,0 +1,11 @@
+const users = {
+  name: "John",
+  age: 30,
+  job: "Engineer",
+};
+const keys = Object.keys(users);
+const vaules = Object.values(users);
+const entries = Object.entries(users);
+console.log(entries);
+console.log(keys);
+console.log(vaules);
