a1d5a9ccea94550a833ac3f465460a073392a00f
Rubel <mr.adcadre41@gmail.com>
2026-02-04 10:41:53 +0600

    notations js added

---
+++ notation.js
@@ -0,0 +1,6 @@
+// dot notation
+const obj = { name: "Alice", age: 33, ocupations: "jobless", 33: "3 ans" };
+console.log(obj.name);
+const studentsProfile = obj.name;
+console.log(obj[33]);
+console.log(studentsProfile);
